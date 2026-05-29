8c560614d84d8548d74b1542833ce7b6933933bd
sagarikam039 <sagarikam039@gmal.com>
2026-05-29 09:05:32 -0400

    Added GitHub Actions CI pipeline

--- banking_app/settings.py
+++ banking_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+
 from dotenv import load_dotenv
 import os
 from pathlib import Path
