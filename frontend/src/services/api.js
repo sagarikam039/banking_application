cf8ca58847ea609e756397656b7d22e98c8008d7
sagarikam039 <sagarikam039@gmal.com>
2026-06-03 15:00:26 -0400

    Fixed CORS on react

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://banking-application-1-vl9z.onrender.com/accounts";
+const API_BASE_URL = "https://banking-application-2-vl9z.onrender.com/accounts";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
