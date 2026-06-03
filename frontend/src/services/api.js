bae6c08f22bb26b819de6e82d63516631db19466
sagarikam039 <sagarikam039@gmal.com>
2026-06-03 13:50:23 -0400

    Added React Frontend Application

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/accounts";
+const API_BASE_URL = "https://banking-application-1-vl9z.onrender.com/accounts";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
