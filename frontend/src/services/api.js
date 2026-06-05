e444eb543f73abb44ed26e56afa5ae39cdf39526
sagarikam039 <sagarikam039@gmal.com>
2026-06-05 13:04:15 -0400

    Temperarily hardcoded backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://banking-application-1-vl9z.onrender.com/accounts",
 });
 
 export default api;
